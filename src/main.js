6c705b8ab3615c6726b528aeeaab00f724a7549d
JuanGiacalone <juanpablogiacalone8@gmail.com>
2021-04-24 23:54:40 -0300

    finished build route

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 /* eslint-disable */ 
+
+import router from './router'
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");
